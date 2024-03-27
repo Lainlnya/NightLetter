d7eb19c7e72fea66f915f7b3d32a8f321e0c5c59
namjun <wrasf175@naver.com>
2024-03-27 15:42:18 +0900

    feature : wrap layout.tsx with RQProvider

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.scss";
+import RQProvider from "./_components/RQProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <RQProvider>
+        <body>{children}</body>
+      </RQProvider>
     </html>
   );
 }
