4ee9452aa36b4ce4e7e0ac61d91de1d0db56189b
Lainlnya <kimyelim100@gmail.com>
2024-03-27 22:08:37 +0900

    [S10P22A108-238] feat: close calendar when outside clicks

--- front-end/src/app/_components/home/MainPage.tsx
+++ front-end/src/app/_components/home/MainPage.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import styles from "./mainPage.module.scss";
-import alarm from "../../../../public/Icons/alarm_icon.svg";
-import calender from "../../../../public/Icons/calender_icon.svg";
-import useStore from "@/store/date";
-import CardSlider from "./CardSlider";
-import { Messages } from "@/utils/msg";
-import { useState } from "react";
-import CalendarComponent from "../diaries/Calendar";
+import Image from 'next/image';
+import styles from './mainPage.module.scss';
+import alarm from '../../../../public/Icons/alarm_icon.svg';
+import calender from '../../../../public/Icons/calender_icon.svg';
+import useStore from '@/store/date';
+import CardSlider from './CardSlider';
+import { Messages } from '@/utils/msg';
+import { useEffect, useRef, useState } from 'react';
+import CalendarComponent from '../diaries/Calendar';
 
 // TODO:
 // 1.서버 데이터 받아서 날짜 동적으로 수정
@@ -19,6 +19,22 @@ import CalendarComponent from "../diaries/Calendar";
 export default function Home() {
   const { date } = useStore();
   const [isSeen, setIsSeen] = useState<boolean>(false);
+  const calendarRef = useRef(null);
+
+  useEffect(() => {
+    // useRef를 통해 안전하게 DOM요소에 접근이 가능
+    function handleTouchOutside(e) {
+      if (calendarRef.current && !calendarRef.current.contains(e.target)) {
+        setIsSeen(false);
+      }
+    }
+    // 클릭 이벤트 리스너 추가
+    document.addEventListener('touchstart', handleTouchOutside);
+    return () => {
+      // 언마운트 시 이벤트 리스너 제거
+      document.removeEventListener('touchstart', handleTouchOutside);
+    };
+  }, [calendarRef]);
 
   return (
     <>
@@ -26,20 +42,18 @@ export default function Home() {
         <div className={styles.header_icons}>
           <Image
             src={alarm}
-            alt="alarm"
+            alt='alarm'
             width={24}
             height={24}
             className={styles.header_icon}
             onClick={() => setIsSeen(true)}
           />
-          <Image
-            src={calender}
-            alt="calender"
-            width={24}
-            height={24}
-            className={styles.header_icon}
-          />
-          {isSeen && <CalendarComponent />}
+          <Image src={calender} alt='calender' width={24} height={24} className={styles.header_icon} />
+          {isSeen && (
+            <div ref={calendarRef}>
+              <CalendarComponent />
+            </div>
+          )}
         </div>
         <div className={styles.header_title}>
           <h1>{date}</h1>
