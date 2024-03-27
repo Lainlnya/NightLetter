03f7d5ee51d9ac9c4dd447555ecdbf206ec89ca8
namjun <wrasf175@naver.com>
2024-03-27 17:59:04 +0900

    chore : add datetime conversion function

--- front-end/src/app/_components/home/MainPage.tsx
+++ front-end/src/app/_components/home/MainPage.tsx
@@ -1,8 +1,8 @@
 import CardSlider from "./CardSlider";
 import Header from "@/app/_components/home/Header";
 import { Messages } from "@/utils/msg";
-import RQProvider from "../RQProvider";
 import styles from "./mainPage.module.scss";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 
 // TODO: 
 // 1.서버 데이터 받아서 날짜 동적으로 수정
@@ -10,14 +10,20 @@ import styles from "./mainPage.module.scss";
 // 3. 아이콘 클릭시 캘린더, 알림페이지로 이동하기
 // 4. 조건에 따라 알림 띄우기
 
-export default function Home() {
+export default async function Home() {
+    const queryClient = new QueryClient();
+    await queryClient.prefetchQuery({ queryKey: ['posts', 'recommends'], queryFn: () => fetch('/api/posts/recommends').then(res => res.json()) });
+    const dehydrateState = dehydrate(queryClient);
+
     return <main className={styles.root}>
         <Header />
         <section className={styles.section}>
             <div className={styles.guide}>
                 {Messages.MAIN_PAGE_DRAG_GUIDE}
             </div>
-            <CardSlider />
+            <HydrationBoundary state={dehydrateState}>
+                <CardSlider />
+            </HydrationBoundary>
         </section>
     </main>;
 }
--- front-end/src/utils/dateFormat.ts
+++ front-end/src/utils/dateFormat.ts
@@ -6,3 +6,21 @@ export function parseDateToKoreanFormatWithDay(day = 0) {
 
     return `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`;
 }
+export function convertDateFormat(dateString: string) {
+    // "YYYY년 mm월 dd일" 형태의 문자열을 "YYYY", "mm", "dd"로 분리합니다.
+    const parts = dateString.match(/(\d{4})년 (\d{1,2})월 (\d{1,2})일/);
+
+    // 분리된 각 부분을 'YYYY-mm-dd' 형식으로 조합합니다.
+    // parts[0]은 전체 일치 문자열이므로, parts[1]부터 사용합니다.
+    // 일이 한 자리 숫자일 경우 앞에 0을 붙여 두 자리로 만듭니다.
+    // 월은 한 자리일 때 그대로 사용합니다.
+    if (parts) {
+        const year = parts[1];
+        const month = parts[2]; // 월은 한 자리일 때 변경 없음
+        const day = parts[3];
+        return `${year}-${month}-${day}`;
+    } else {
+        // 일치하는 형식이 없을 경우 오류 메시지를 반환합니다.
+        return '입력 형식이 올바르지 않습니다.';
+    }
+}
