85296984272767fcc2b68bf63c9231b5eab4e2d3
김영섭 <kimyoungseop33@gmail.com>
2024-04-01 02:34:46 +0900

    feat: add past-tarot draw, find with redis.
    Former-commit-id: 62397c2257132802e02a41d469fb68f336fd1d14

--- back-end/src/main/java/com/nightletter/domain/tarot/service/TarotServiceImpl.java
+++ back-end/src/main/java/com/nightletter/domain/tarot/service/TarotServiceImpl.java
@@ -1,26 +1,41 @@
 package com.nightletter.domain.tarot.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Random;
+import java.util.TimeZone;
 
+import org.springframework.cglib.core.Local;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.nightletter.domain.diary.dto.EmbedVector;
+import com.nightletter.domain.member.entity.Member;
 import com.nightletter.domain.tarot.dto.TarotDto;
 import com.nightletter.domain.tarot.dto.TarotKeyword;
 import com.nightletter.domain.tarot.dto.TarotListResponse;
+import com.nightletter.domain.tarot.entity.PastTarot;
 import com.nightletter.domain.tarot.entity.Tarot;
+import com.nightletter.domain.tarot.entity.TarotDirection;
+import com.nightletter.domain.tarot.repository.TarotRedisRepository;
 import com.nightletter.domain.tarot.repository.TarotRepository;
 
 import jakarta.annotation.PostConstruct;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -28,11 +43,12 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class TarotService {
+public class TarotServiceImpl implements TarotService {
 
 	private static final Map<Integer, TarotDto> deck = new HashMap<>();
 	private final TarotRepository tarotRepository;
 	private final WebClient webClient;
+	private final TarotRedisRepository tarotRedisRepository;
 
 	@PostConstruct
 	private void getTarotEmbedded() {
@@ -122,5 +138,49 @@ public class TarotService {
 		} while (isIgnored);
 
 		return id;
+	}
+
+	@Override
+	public Optional<TarotDto> createRandomPastTarot() {
+		// 과거 카드 redis 저장.
+		// redisTemplate.
+		Integer tarotId = new Random().nextInt(1, 157);
+
+		// 임시로 1번 카드로 고정.
+		tarotId = 1;
+		Optional<Tarot> tarotResponse = tarotRepository.findById(tarotId);
+
+		if (tarotResponse.isEmpty()) return Optional.empty();
+
+		LocalDateTime expiredTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(4, 0));
+
+		if (LocalTime.now().isAfter(LocalTime.of(4, 0))) {
+			expiredTime = expiredTime.plusDays(1);
+		}
+
+		PastTarot pastTarot = tarotRedisRepository.save(PastTarot.builder()
+			.memberId(getCurrentMemberId())
+			.tarotId(tarotId)
+			.expiredTime(expiredTime.toEpochSecond(ZoneOffset.UTC)
+				- LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)
+			)
+			.direction(tarotResponse.get().getDir())
+			.build()
+		);
+
+		return tarotResponse.map(tarot -> TarotDto.of(tarot, tarot.getDir()));
+	}
+
+	@Override
+	public Optional<TarotDto> getRandomPastTarot() {
+		Optional<PastTarot> pastTarot = tarotRedisRepository.findById(getCurrentMemberId());
+
+		return pastTarot.flatMap(
+			value -> tarotRepository.findById(value.getTarotId()).map(tarot -> TarotDto.of(tarot, tarot.getDir())));
+	}
+
+	private Integer getCurrentMemberId() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return Integer.parseInt((String) authentication.getPrincipal());
 	}
 }
