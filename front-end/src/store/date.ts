d9ad8c56c37707e414dbd502a9ffbd53de7b6c05
namjun <wrasf175@naver.com>
2024-04-01 18:00:01 +0900

    [S10P22A108-123] feat : fix default date

--- front-end/src/store/date.ts
+++ front-end/src/store/date.ts
@@ -2,14 +2,14 @@ import { create } from 'zustand';
 import { parseDateToKoreanFormatWithDay } from '../utils/dateFormat';
 
 interface DateState {
-    date: string;
+    date: string | null;
     daysDifference: number;
     setDate: (date: string) => void;
     setDaysDifference: (daysDifference: number) => void;
 }
 
 const useStore = create<DateState>(set => ({
-    date: parseDateToKoreanFormatWithDay(),
+    date: null,
     daysDifference: 0,
     setDate: (date: string) => set({ date }),
     setDaysDifference: (daysDifference: number) => set({ daysDifference })
