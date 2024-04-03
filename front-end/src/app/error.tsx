55b00948b30c96566bd4dd17d1f899281f9f3e21
Lainlnya <kimyelim100@gmail.com>
2024-04-03 16:29:51 +0900

    feat: add error page & notfound page
    Former-commit-id: b5b222b2c26e16cf1c3113f4ee6ca848788625f6

---
+++ front-end/src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const ErrorPage = () => {
+  return <div>예상치 못한 오류가 발생했습니다.</div>;
+};
+
+export default ErrorPage;
---
+++ front-end/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div>요청하신 페이지는 존재하지 않습니다.</div>;
+};
+
+export default NotFound;
