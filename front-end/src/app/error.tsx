47c6df692685d81befe63260ca8758eb5e1c7060
Lainlnya <kimyelim100@gmail.com>
2024-04-03 16:47:29 +0900

    design: change not-found page & error page
    Former-commit-id: 541ed670a9ef6df75f6d29ba7d1abaafc1e24777

--- front-end/src/app/error.tsx
+++ front-end/src/app/error.tsx
@@ -1,9 +1,15 @@
 "use client";
-
+import Link from "next/link";
+import styles from "./error.module.scss";
 import React from "react";
 
 const ErrorPage = () => {
-  return <div>예상치 못한 오류가 발생했습니다.</div>;
+  return (
+    <main className={styles.main}>
+      <h1>예상치 못한 오류가 발생했습니다.</h1>
+      <Link href="/">메인 페이지로 이동하기</Link>
+    </main>
+  );
 };
 
 export default ErrorPage;
--- front-end/src/app/not-found.tsx
+++ front-end/src/app/not-found.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./not-found.module.scss";
+import Link from "next/link";
 
 const NotFound = () => {
-  return <div>요청하신 페이지는 존재하지 않습니다.</div>;
+  return (
+    <main className={styles.main}>
+      <h1>요청하신 페이지는 존재하지 않습니다.</h1>
+      <Link href="/">메인 페이지로 이동하기</Link>
+    </main>
+  );
 };
 
 export default NotFound;
