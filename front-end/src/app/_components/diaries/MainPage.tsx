e21d18b0be85307772d7fbf571086919fc4a7e01
Lainlnya <kimyelim100@gmail.com>
2024-04-03 22:47:40 +0900

    hotfix: add console for debug
    Former-commit-id: aa33c9033b39e9af44c8a9d5f9bfd1328981cd04

--- front-end/src/app/_components/diaries/MainPage.tsx
+++ front-end/src/app/_components/diaries/MainPage.tsx
@@ -10,7 +10,10 @@ import { motion, useMotionValue } from "framer-motion";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { DRAG_BUFFER } from "@/utils/animation";
-import { convertDateFormat, convertDateFormatToKorean } from "@/utils/dateFormat";
+import {
+  convertDateFormat,
+  convertDateFormatToKorean,
+} from "@/utils/dateFormat";
 import { useQuery } from "@tanstack/react-query";
 import getInitialDiaries from "@/libs/getInitialDiaries";
 import { DiaryEntry } from "@/types/card";
@@ -19,25 +22,35 @@ import useStore from "@/store/date";
 export default function MainPage() {
   const router = useRouter();
   const { date, setDate } = useStore();
-  const { data } = useQuery({ queryKey: ["diary", "diaries"], queryFn: () => getInitialDiaries(convertDateFormat(date)) });
+  const { data } = useQuery({
+    queryKey: ["diary", "diaries"],
+    queryFn: () => getInitialDiaries(convertDateFormat(date)),
+  });
 
   const [dragging, setDragging] = useState(false);
   const [cardIndex, setCardIndex] = useState(data?.requestDiaryIdx);
 
-
   const dragX = useMotionValue(0);
 
   useEffect(() => {
-    setDate(convertDateFormatToKorean(data?.diaries?.[data?.requestDiaryIdx]?.date + 1));
-  }, [])
+    setDate(
+      convertDateFormatToKorean(
+        data?.diaries?.[data?.requestDiaryIdx]?.date + 1
+      )
+    );
+    if (data) {
+      console.log("main page useEffect: " + data);
+    }
+  }, []);
 
   useEffect(() => {
     setDate(convertDateFormatToKorean(data?.diaries?.[cardIndex]?.date));
-  }, [cardIndex])
-
+    if (data) {
+      console.log("main page2 useEffect: " + data);
+    }
+  }, [cardIndex]);
 
   console.log(data);
-
 
   const onDragStart = () => {
     setDragging(true);
@@ -50,8 +63,6 @@ export default function MainPage() {
 
     if (x <= -DRAG_BUFFER && cardIndex < data?.diaries?.length - 1) {
       setCardIndex((prev: number) => prev + 1);
-
-
     } else if (x >= DRAG_BUFFER && cardIndex > 0) {
       setCardIndex((prev: number) => prev - 1);
     }
@@ -118,9 +129,7 @@ export default function MainPage() {
                 </div>
                 <div className={styles.diary_text}>
                   <h2>오늘의 일기</h2>
-                  <p>
-                    {diary.content}
-                  </p>
+                  <p>{diary.content}</p>
                 </div>
               </main>
             );
--- front-end/src/utils/dateFormat.ts
+++ front-end/src/utils/dateFormat.ts
@@ -1,27 +1,36 @@
 export function parseDateToKoreanFormatWithDay(day = 0) {
   const today = new Date();
 
-  if (day !== 0)
-    today.setDate(today.getDate() + day);
+  if (day !== 0) today.setDate(today.getDate() + day);
 
-  return `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`;
+  return `${today.getFullYear()}년 ${
+    today.getMonth() + 1
+  }월 ${today.getDate()}일`;
 }
 
-
 export function convertDateFormat(dateString: string | null) {
-  const parts = dateString && dateString.match(/(\d{4})년 (\d{1,2})월 (\d{1,2})일/);
+  if (dateString) {
+    console.log("convertDateFormat: " + dateString);
+  }
+
+  const parts =
+    dateString && dateString.match(/(\d{4})년 (\d{1,2})월 (\d{1,2})일/);
 
   if (parts) {
     const year = parts[1];
-    const month = parts[2].padStart(2, '0');
-    const day = parts[3].padStart(2, '0');
+    const month = parts[2].padStart(2, "0");
+    const day = parts[3].padStart(2, "0");
     return `${year}-${month}-${day}`;
   } else {
-    return '입력 형식이 올바르지 않습니다.';
+    return "입력 형식이 올바르지 않습니다.";
   }
 }
 
 export function convertDateFormatToKorean(dateString: string) {
+  if (dateString) {
+    console.log("convertDateFormatToKorean: " + dateString);
+  }
+
   const parts = dateString && dateString.match(/(\d{4})-(\d{1,2})-(\d{1,2})/);
 
   if (parts) {
@@ -30,13 +39,21 @@ export function convertDateFormatToKorean(dateString: string) {
     const day = parts[3];
     return `${year}년 ${month}월 ${day}일`;
   } else {
-    return '입력 형식이 올바르지 않습니다.';
+    return "입력 형식이 올바르지 않습니다.";
   }
 }
 
 export function getTodayDate() {
   var d = new Date();
-  return d.getFullYear() + "-" + ((d.getMonth() + 1) > 9 ? (d.getMonth() + 1).toString() : "0" + (d.getMonth() + 1)) + "-" + (d.getDate() > 9 ? d.getDate().toString() : "0" + d.getDate().toString());
+  return (
+    d.getFullYear() +
+    "-" +
+    (d.getMonth() + 1 > 9
+      ? (d.getMonth() + 1).toString()
+      : "0" + (d.getMonth() + 1)) +
+    "-" +
+    (d.getDate() > 9 ? d.getDate().toString() : "0" + d.getDate().toString())
+  );
 }
 
 export const getDateDiff = (d1: string | Date, d2: string | Date) => {
@@ -46,7 +63,7 @@ export const getDateDiff = (d1: string | Date, d2: string | Date) => {
   console.log(d2);
   const diffDate = date1.getTime() - date2.getTime();
   return Math.abs(diffDate / (1000 * 60 * 60 * 24)); // 밀리초 * 초 * 분 * 시 = 일
-}
+};
 
 export const isToday = (date1: string, data2: string) => date1.match(data2);
 
