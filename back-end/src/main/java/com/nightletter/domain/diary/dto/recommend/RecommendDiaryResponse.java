4b81851393c47ccf843b3ca4369925685903b533
olrlobt <317tmdgjs@naver.com>
2024-04-04 13:36:47 +0900

    !HOTFIX
    Former-commit-id: a582419f559ab51973bfd05f09a0799adcbc8765

--- back-end/src/main/java/com/nightletter/domain/diary/dto/recommend/RecommendDiaryResponse.java
+++ back-end/src/main/java/com/nightletter/domain/diary/dto/recommend/RecommendDiaryResponse.java
@@ -1,10 +1,14 @@
 package com.nightletter.domain.diary.dto.recommend;
 
+import java.util.Objects;
+
 import com.nightletter.global.utils.Nickname;
 
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class RecommendDiaryResponse {
 
 	String nickname;
@@ -16,4 +20,21 @@ public class RecommendDiaryResponse {
 		this.imgUrl = imgUrl;
 		this.nickname = Nickname.createRandom();
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		RecommendDiaryResponse that = (RecommendDiaryResponse)o;
+		return Objects.equals(content, that.content) &&
+			Objects.equals(imgUrl, that.imgUrl);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(content, imgUrl);
+	}
+
 }
