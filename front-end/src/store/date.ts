a21290b7a2eb7958d148471154c31c9e414ca886
namjun <wrasf175@naver.com>
2024-05-02 10:35:25 +0900

    feat : 로그인 전역상태로 관리

--- front-end/src/store/date.ts
+++ front-end/src/store/date.ts
@@ -4,15 +4,19 @@ import { create } from 'zustand';
 interface DateState {
     date: string;
     daysDifference: number;
+    username: string | null;
     setDate: (date: string) => void;
     setDaysDifference: (daysDifference: number) => void;
+    setUserName: (username: string) => void;
 }
 
 const useStore = create<DateState>(set => ({
     date: "",
+    username: null,
     daysDifference: 0,
     setDate: (date: string) => set({ date }),
-    setDaysDifference: (daysDifference: number) => set({ daysDifference })
+    setDaysDifference: (daysDifference: number) => set({ daysDifference }),
+    setUserName: (username: string) => set({ username })
 }));
 
 export default useStore;
