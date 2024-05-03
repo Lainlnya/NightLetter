d0b0936d2dbe2444c1a825a72a8b07b9a8907cfa
Lainlnya <kimyelim100@gmail.com>
2024-05-03 10:44:43 +0900

    feat: add GNB router

--- front-end/src/app/_components/common/GNB.tsx
+++ front-end/src/app/_components/common/GNB.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import home from "../../../../public/icons/house-solid.svg";
 import envelope from "../../../../public/icons/envelope-solid.svg";
@@ -5,32 +8,42 @@ import chat from "../../../../public/icons/comment-solid.svg";
 import profile from "../../../../public/icons/user-solid.svg";
 import styles from "./GNB.module.scss";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faComment,
+  faEnvelope,
+  faHouse,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+// import {}
 
 const GNB = () => {
+  const pathname = usePathname();
+
   return (
     <nav className={styles.nav}>
       <ul>
-        <Link href="/">
-          <li>
-            <Image src={home} alt="home icon" width={29} height={26} />
+        <Link href='/'>
+          <li className={`${pathname === "/" ? styles.active : ""}`}>
+            <FontAwesomeIcon icon={faHouse} />
             <span>HOME</span>
           </li>
         </Link>
-        <Link href="/scrap">
-          <li>
-            <Image src={envelope} alt="home icon" width={29} height={26} />
+        <Link href='/scrap'>
+          <li className={`${pathname === "/scrap" ? styles.active : ""}`}>
+            <FontAwesomeIcon icon={faEnvelope} />
             <span>스크랩</span>
           </li>
         </Link>
-        <Link href="/chatting">
-          <li>
-            <Image src={chat} alt="home icon" width={29} height={26} />
+        <Link href='/chatting'>
+          <li className={`${pathname === "/chatting" ? styles.active : ""}`}>
+            <FontAwesomeIcon icon={faComment} />
             <span>CHAT</span>
           </li>
         </Link>
-        <Link href="/profile">
-          <li>
-            <Image src={profile} alt="home icon" width={29} height={26} />
+        <Link href='/profile'>
+          <li className={`${pathname === "/profile" ? styles.active : ""}`}>
+            <FontAwesomeIcon icon={faUser} />
             <span>PROFILE</span>
           </li>
         </Link>
