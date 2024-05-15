3ec1e238d75cca08b5ec10a48324266d43d8f7b9
김영섭 <kimyoungseop33@gmail.com>
2024-05-15 20:06:14 +0900

    fix: chat test profile img path

--- back-end/src/main/java/com/nightletter/domain/social/api/ChatController.java
+++ back-end/src/main/java/com/nightletter/domain/social/api/ChatController.java
@@ -2,7 +2,9 @@ package com.nightletter.domain.social.api;
 
 import java.time.LocalDateTime;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,6 +16,9 @@ import com.nightletter.domain.social.dto.response.ChatResponse;
 
 @Controller
 public class ChatController {
+
+	@Value("${chat.test-profile}")
+	private String testProfileImg;
 
 	@MessageMapping("/{roomId}")
 	@SendTo("/room/{roomId}")
@@ -28,7 +33,7 @@ public class ChatController {
 				.chatId(-1L)
 				.message(request.getMessage())
 				.nickname("예롬예롬")
-				.profileImgUrl("https://i.namu.wiki/i/MCV0dofhJ6D7afd6Ajr2ZK3jXTm9fGKUcBpwNSfaz-cJWDbvtNodOd_Paqrmh7C49U4L3MAPYoE_PKAJtMKlpCsP0gnJ3n833LcCobsfgsu4S1a2HELeLZM9ZreFC-NhtD3lpyMlOgSW8idLWgC9FA.webp")
+				.profileImgUrl(testProfileImg + ThreadLocalRandom.current().nextInt(0, 4) + ".jpg")
 				.senderId(3)
 				.sendTime(LocalDateTime.now())
 				.sentByMe(new Random().nextBoolean())
