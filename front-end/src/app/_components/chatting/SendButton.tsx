5b7ec108f0ee127b3e0f8d357b9ad0980de18e31
Lainlnya <kimyelim100@gmail.com>
2024-05-18 00:19:24 +0900

    fix: Send Button props

--- front-end/src/app/_components/chatting/SendButton.tsx
+++ front-end/src/app/_components/chatting/SendButton.tsx
@@ -1,12 +1,18 @@
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const SendButton = ({ newMessage, setNewMessage, sendMessage }) => {
+type SendButtonProps = {
+  newMessage: string;
+  setNewMessage: (message: string) => void;
+  sendMessage: () => void;
+};
+
+const SendButton: React.FC<SendButtonProps> = ({ newMessage, setNewMessage, sendMessage }) => {
   return (
     <>
       <input
-        style={{ color: "black" }}
-        type='text'
+        style={{ color: 'black' }}
+        type="text"
         value={newMessage}
         onChange={(e) => setNewMessage(e.target.value)}
       />
