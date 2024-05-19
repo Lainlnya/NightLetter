60091e4579e9e09fe262dfca8af98c70cb1d0075
김영섭 <kimyoungseop33@gmail.com>
2024-05-19 22:05:18 +0900

    merge: merge from develop-v2-back and add docker-compose.yml env

--- back-end/src/main/java/com/nightletter/global/security/handler/jwt/JwtAuthenticationFilter.java
+++ back-end/src/main/java/com/nightletter/global/security/handler/jwt/JwtAuthenticationFilter.java
@@ -34,15 +34,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private final MemberRepository memberRepository;
 	private final JwtProvider jwtProvider;
 
-	@Value("${spring.security.dev-token}")
-	private String devToken;
-
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
 		try {
 			// 토큰 확인.
-			String token = devToken;
+			String token = parseBearerToken(request);
 
 			if (token == null) {
 				filterChain.doFilter(request, response);
