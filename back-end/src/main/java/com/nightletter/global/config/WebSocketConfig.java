12fa13db7d56f3af35c94b4599d8129b7851def5
김영섭 <kimyoungseop33@gmail.com>
2024-05-20 01:38:46 +0900

    fix: wws security context

--- back-end/src/main/java/com/nightletter/global/config/WebSocketConfig.java
+++ back-end/src/main/java/com/nightletter/global/config/WebSocketConfig.java
@@ -46,7 +46,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws-stomp")
-			.addInterceptors(interceptor)
 			.setAllowedOrigins("http://localhost:5500", "https://localhost:3001", "*");
 	}
 
