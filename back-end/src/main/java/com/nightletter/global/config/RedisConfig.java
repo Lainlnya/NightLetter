9908824ba28306d52a88a5127063d3105369b755
김영섭 <kimyoungseop33@gmail.com>
2024-05-19 17:22:54 +0900

    fix: redis container connection info (password)

--- back-end/src/main/java/com/nightletter/global/config/RedisConfig.java
+++ back-end/src/main/java/com/nightletter/global/config/RedisConfig.java
@@ -1,23 +1,39 @@
 package com.nightletter.global.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 	@Value("${spring.data.redis.host}")
 	private String host;
 
+	@Value("${spring.data.redis.password}")
+	private String password;
+
 	@Value("${spring.data.redis.port}")
 	private int port;
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+
+		config.setHostName(host);
+		config.setPort(port);
+		config.setPassword(password);
+
+		return new LettuceConnectionFactory(config);
 	}
 
 	@Bean
@@ -26,4 +42,5 @@ public class RedisConfig {
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		return redisTemplate;
 	}
+
 }
